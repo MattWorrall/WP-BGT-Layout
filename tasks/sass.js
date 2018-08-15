30f7b30377d9e20a8011b4829f0b751d4427a049
Matt Worrall <mworrall@burning-glass.com>
2018-08-15 17:17:31 +0100

    Added steps to the SCSS build

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,28 +1,60 @@
 const gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    autoprefixer = require('gulp-autoprefixer');
+  sass = require('gulp-sass'),
+  sourceMaps = require('gulp-sourcemaps'),
+  autoprefixer = require('gulp-autoprefixer'),
+  sassLint = require('gulp-sass-lint'),
+  sassDoc = require('sassDoc');
 
 const sassOptions = {
-    precision: 8
+  precision: 8
+};
+
+const sassLintOptions = {
+  files: {
+    ignore: 'src/scss/partials/_normalize.scss' // ignore normalize, by its nature it breaks the rules!
+  }
 };
 
 const autoPrefixerOptions = {
-    browsers: [
-        'Android >= 6',
-        'Chrome >= 56',
-        'Firefox >= 51',
-        'Explorer >= 11',
-        'iOS >= 10',
-        'Opera >= 43',
-        'Safari >= 10',
-        'Edge >= 12'
-    ]
+  browsers: [
+    'Android >= 6',
+    'Chrome >= 56',
+    'Firefox >= 51',
+    'Explorer >= 11',
+    'iOS >= 10',
+    'Opera >= 43',
+    'Safari >= 10',
+    'Edge >= 12'
+  ]
+};
+
+const sassDocOptions = {
+  dest: 'docs/css',
+  verbose: true,
+  display: {
+    access: ['public', 'private'],
+    alias: true,
+    watermark: true
+  },
+  package: {
+    title: 'BGT WebApps Layout',
+    name: 'bgt-layout',
+    license: 'Internal Use Only',
+    homepage: 'https://github.com/MattWorrall/WP-BGT-Layout',
+    description: 'Standard layout frame work for BGT web applications'
+  }
 };
 
 gulp.task('build:scss', () => {
-    return gulp.src('./src/scss/bgt-layout.scss')
-        .pipe(sass(sassOptions))
-        .pipe(autoprefixer(autoPrefixerOptions))
-        .pipe(gulp.dest('./dist/css'));
+  return gulp
+    .src('./src/scss/**/*.scss')
+    .pipe(sassLint(sassLintOptions))
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+    .pipe(sassDoc(sassDocOptions)) // TODO move this to release:scss task when created
+    .pipe(sourceMaps.init())
+    .pipe(sass(sassOptions))
+    .pipe(autoprefixer(autoPrefixerOptions))
+    .pipe(sourceMaps.write('./'))
+    .pipe(gulp.dest('./dist/css'));
 });
